140387c5e145efb02f0cb70f4f06f45ac68640d8
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-06 17:50:30 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -240,7 +240,7 @@ def AiMG():
 
 st.image(_image,use_column_width='auto')
 
-keyy = st.text_input("Set Token","apl3jkhuisfdbw328jkyhf73",type="password",help="Enter a Token to use our Ai System")
+keyy = st.text_input("Set Black-Key Code","BBW-apl3jkhuisfdbw328jkyhf73",type="password",help="Enter a Token to use our Ai System")
 
 def obfuscate(text): 
     result = ""
