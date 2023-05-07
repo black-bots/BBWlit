369968b88cf9c759cef35148c99459fc604e5052
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-07 16:12:30 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,91 +1,36 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+idef my_widget(key):
+    st.subheader('Hello there!')
+    return st.button("Click me " + key)
 
-#st.set_page_config(page_title=None, page_icon=None, layout="wide")
+# This works in the main area
+clicked = my_widget("first")
 
-# function to make any grid
+# And within an expander
+my_expander = st.expander("Expand", expanded=True)
+with my_expander:
+    clicked = my_widget("second")
 
-def make_grid(cols,rows):
-    grid = [0]*cols
-    for i in range(cols):
-        with st.container():
-            grid[i] = st.columns(rows)
-    return grid
+# AND in st.sidebar!
+with st.sidebar:
+    clicked = my_widget("third")
+    
+#############################
+st.title("Let's create a table!")
+for i in range(1, 10):
+    cols = st.columns(4)
+    cols[0].write(f'{i}')
+    cols[1].write(f'{i * i}')
+    cols[2].write(f'{i * i * i}')
+    cols[3].write('x' * i)
+################################
+col1, col2 = st.columns(2)
 
-# Main layout begins here
+original = Image.open(image)
+col1.header("Original")
+col1.image(original, use_column_width=True)
 
-st.title("""
-Three key findings from the 2022 UN Population Prospects
-""")
-
-st.write("""
-The UN releases an update of its World Population Prospects every two years. 
-Its latest release was delayed due to the COVID-19 pandemic and was released in 2022.
-""")
-
-st.markdown("---")
-        
-# Make the grid
-mygrid = make_grid(3,(2,4,4))
-
-# Row 0
-popgrowth = 2
-worldgrowth = popgrowth[popgrowth['Country name'] == 'World']
-
-fig, ax = plt.subplots()
-worldgrowth.plot(x='Year', y='Population growth rate', ax=ax)
-
-mygrid[0][0].markdown("""
-#### World population could be 8 billion by end of 2022
-""")
-
-mygrid[0][1].write("""
-Since 1975 the world has been growing by billion people every 12 years. 
-It passed 7 billion in 2011 and, by the end of 2022, 
-there will be 8 billion people in the world. 
-But, the growth rate is below 1%, less than half its peak rate of growth - of 2.3% - in the 1960s.
-""")
-
-mygrid[0][2].pyplot(fig)
-
-# Row 1
-popdf = "popdf"
-worldpop = popdf[popdf['Country name'] == 'World']
-
-fig, ax = plt.subplots()
-worldpop.plot(x='Year',y='Population', ax=ax)
-
-mygrid[1][0].markdown("""
-#### World population will peak at 10.4 billion in 2086
-""")
-
-mygrid[1][1].write("""
-The world population has increased rapidly over the last century.
-The UN projects that the global population will peak before the end of the century,
- in 2086, at just over 10.4 billion people.
-""")
-
-mygrid[1][2].pyplot(fig)
-
-# Row 2
-fig, ax = plt.subplots()
-ax = popdf[popdf['Country name'] == 'India'].plot(x = 'Year',y = 'Population', label = 'India', ax=ax)
-popdf[popdf['Country name'] == 'China'].plot(x = 'Year', label = "China", y='Population', ax = ax)
-
-mygrid[2][0].markdown("""
-#### In 2023 India will overtake China as the world's most populous country
-""")
-mygrid[2][1].write("""
-China is the world's most populous with more than 1.4 billion people. 
-Now, its growth rate has fallen due to a rapid drop in fertility rate 
-over the 1970s and 80s.
-In India, the rate of decline has been slower, so it is expected to overtake China in 2023.
-""")
-mygrid[2][2].pyplot(fig)
-
-# Footer
-st.markdown("---")
-st.markdown("""_Data and text courtesy of, 
-[Our World in Data](https://ourworldindata.org/world-population-update-2022)_,
- [CC BY 4](https://creativecommons.org/licenses/by/4.0/)""")
+grayscale = original.convert('LA')
+col2.header("Grayscale")
+col2.image(grayscale, use_column_width=True)
