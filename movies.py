50e4ccfb58a349edbcfff781b884a4c817b0cd81
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-07 16:15:34 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-image = Image.open('static/-.ico')
+image = Image.open('static/1.png')
 def my_widget(key):
     st.subheader('Hello there!')
     return st.button("Click me " + key)
