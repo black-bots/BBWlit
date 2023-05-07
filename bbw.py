60e7a3bdd536805b899757ddbb1e341aac8d6c26
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-07 02:56:05 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -274,7 +274,9 @@ with st.sidebar:
 			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
+			Click <a href="https://black-bots.github.io/pay.html">here</a> if button not working
 		"""
+		
 
 		st.markdown(html_string, unsafe_allow_html=True)
 
