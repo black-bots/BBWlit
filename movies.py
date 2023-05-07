e11895c89ad661e042f75096f3a2d984ebdb25de
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-07 16:37:29 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -5,11 +5,8 @@ from PIL import Image
 image = Image.open('static/ori_3748732_mkqg8a0irpybxk8xtejrqetwi9j3f3011wq3dyi1_online-cinema-banner-vector-realistic-computer-monitor-movie-brochure-design-template-banner-for-movie-premiere-show-marketing-luxury-poster-illustration.jpg')
 st.image(image,use_column_width=True)
 
-def my_widget(key):
-    st.subheader('Hello there! tavos')
-    return st.button("Click me " + key)
 def my_widget2(key):
-    st.subheader('gooodbyeees')
+    st.subheader('Watch Now')
     return st.button("Click me " + key)
 
 #############################
@@ -23,9 +20,8 @@ with my_expander:
         cols[3].write('John Wick: Chapter 4')
         cols[4].write(image)
         cols[5].write(my_widget2('-'))
-        
-# This works in the main area
-clicked = my_widget("first")
 ################################
 with st.sidebar:
-    clicked = my_widget("third")
+        cols = st.columns(2)
+        cols[0].write('the Super Mario Movie')
+        cols[1].write('Evil Dead Rise')
