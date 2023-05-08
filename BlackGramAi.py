c23f37cdb088a6cfe784c03d340798995e2a94d7
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-08 12:32:55 -0400

    Update BlackGramAi.py

--- BlackGramAi.py
+++ BlackGramAi.py
@@ -1,34 +1,32 @@
+import streamlit as st
 import instaloader
 
-username = 'Put your username inside this quote'
-password = 'Put your password inside this quote'
-
+st.header("Your friendly neighborhood Instabot")
+name = st.text_input("Enter user name")
+password = st.text_input("Enter password")
 L = instaloader.Instaloader()
-# Login or load session
 
-L.login(username, password)#this should be your own username and #password
-profile = instaloader.Profile.from_username(L.context, username)
+if st.button('Get Ghost list'):
+    # Login or load session
+    L.login(name, password)        # (login)
 
-# in the line above, replace the username field by the username of 
-#the person you want this bot to work for. If you want to find the 
-#list of persons who do not follow you back, replace username by 
-#your own username. If you want for someone else, use their username.
-
+profile = instaloader.Profile.from_username(L.context, name)
 main_followers = profile.followers
-follower_list = [] #people who follow you
+
+follower_list = []
 for person in profile.get_followers():
   user_id = person.userid
   follower_list.append(person.username)
-
+  
 main_followees = profile.followees
-followee_list = [] #people whom you follow
+followee_list = []
 for person in profile.get_followees():
   user_id = person.userid
   followee_list.append(person.username)
-
+  
 req = []
 for element in followee_list:
   if element not in follower_list:
-req.append(element)
-
-print(sorted(req))
+    req.append(element)
+  
+st.write(sorted(req))
