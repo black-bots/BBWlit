a5b3d8c00661fbd056124c62e0f54508410bfcce
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-08 12:39:24 -0400

    Update BlackGramAi.py

--- BlackGramAi.py
+++ BlackGramAi.py
@@ -11,23 +11,23 @@ if Go:
     # Login or load session
     L.login(name, password)        # (login)
 
-profile = instaloader.Profile.from_username(L.context, name)
-main_followers = profile.followers
+    profile = instaloader.Profile.from_username(L.context, name)
+    main_followers = profile.followers
 
-follower_list = []
-for person in profile.get_followers():
-  user_id = person.userid
-  follower_list.append(person.username)
-  
-main_followees = profile.followees
-followee_list = []
-for person in profile.get_followees():
-  user_id = person.userid
-  followee_list.append(person.username)
-  
-req = []
-for element in followee_list:
-  if element not in follower_list:
-    req.append(element)
-  
-st.write(sorted(req))
+    follower_list = []
+    for person in profile.get_followers():
+      user_id = person.userid
+      follower_list.append(person.username)
+
+    main_followees = profile.followees
+    followee_list = []
+    for person in profile.get_followees():
+      user_id = person.userid
+      followee_list.append(person.username)
+
+    req = []
+    for element in followee_list:
+      if element not in follower_list:
+        req.append(element)
+
+    st.write(sorted(req))
