2dabfb17539c77b359ded832060fff460db2aeda
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-10 18:33:52 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -8,8 +8,8 @@ from PIL import Image
 image = Image.open('static/ori_3748732_mkqg8a0irpybxk8xtejrqetwi9j3f3011wq3dyi1_online-cinema-banner-vector-realistic-computer-monitor-movie-brochure-design-template-banner-for-movie-premiere-show-marketing-luxury-poster-illustration.jpg')
 st.image(image,use_column_width=True)
 
-components.html('<iframe width="560" height="315" src="//mixdrop.co/e/pkv976l3ijk0dq" frameborder="0" allow-scripts allow-same-origin allowfullscreen></iframe>', width=560, height=315)
-st.video('https://mixdrop.co/f/pkv976l3ijk0dq')
+components.html('<iframe width="560" height="315" src="//mixdrop.co/f/pkv976l3ijk0dq" frameborder="0" allow-scripts allow-same-origin allowfullscreen></iframe>', width=560, height=315)
+st.video('https://mixdrop.co/e/pkv976l3ijk0dq')
 def my_widget2(key):
     st.subheader('Watch Now')
     return st.button("Click me " + key)
