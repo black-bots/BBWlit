ed2a489e0c52047c8bc27c02b4e68b4a29e0cfb4
black-bots <125762717+black-bots@users.noreply.github.com>
2023-05-23 17:48:46 -0400

    Update book.py

--- book.py
+++ book.py
@@ -429,17 +429,16 @@ if ok:
 
 				col1 = st.columns(1)
 
-
 				with col1:
-				    text_input = st.text_input(
-					"Enter some text 👇",
-					label_visibility=st.session_state.visibility,
-					disabled=st.session_state.disabled,
-					placeholder=st.session_state.placeholder,
-				    )
+					text_input = st.text_input(
+						"Enter some text 👇",
+						label_visibility=st.session_state.visibility,
+						disabled=st.session_state.disabled,
+						placeholder=st.session_state.placeholder,
+					)
 
-				if text_input:
-					st.write("You entered: ", text_input)
+					if text_input:
+						st.write("You entered: ", text_input)
 
 		if ok & selected2:
 
