778e5e05ccb822c997dd0241f205280fe6a98419
black-bots <125762717+black-bots@users.noreply.github.com>
2023-06-29 04:33:33 -0400

    Update NewButler.py

--- NewButler.py
+++ NewButler.py
@@ -32,7 +32,7 @@ session.headers = {
         }
 session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY")) 
 
-bard = Bard(token=token, proxies={'http':'http://127.0.0.8:1080', 'https':'http://127.0.0.8:1080'}, session=session, timeout=30)
+bard = Bard(token=token, proxies={'http':'http://127.0.0.1:1080', 'https':'http://127.0.0.1:1080'}, session=session, timeout=30)
 
 history = []
 from PIL import Image
