cf657db94ae6a0cac746668bf896105ae0829cea
black-bots <125762717+black-bots@users.noreply.github.com>
2023-07-18 10:53:08 -0400

    Update gr.py

--- gr.py
+++ gr.py
@@ -1,4 +1,6 @@
 from instagrapi import Client
+from instagrapi.exceptions import LoginRequired
+import logging
 import pandas as pd
 import numpy as np
 import matplotlib as mp
@@ -8,14 +10,60 @@ import openai
 import prompt_toolkit
 import streamlit as st
 
+cl = Client()
+cl.delay_range = [2, 6]
+def login_user():
+
+    cl = Client()
+    session = cl.load_settings("session.json")
+
+    login_via_session = False
+    login_via_pw = False
+
+    if session:
+        try:
+            cl.set_settings(session)
+            cl.login(USERNAME, PASSWORD)
+
+            # check if session is valid
+            try:
+                cl.get_timeline_feed()
+            except LoginRequired:
+                logger.info("Session is invalid, need to login via username and password")
+
+                old_session = cl.get_settings()
+
+                # use the same device uuids across logins
+                cl.set_settings({})
+                cl.set_uuids(old_session["uuids"])
+
+                cl.login(USERNAME, PASSWORD)
+            login_via_session = True
+        except Exception as e:
+            logger.info("Couldn't login user using session information: %s" % e)
+
+    if not login_via_session:
+        try:
+            logger.info("Attempting to login via username and password. username: %s" % USERNAME)
+            if cl.login(USERNAME, PASSWORD):
+                login_via_pw = True
+        except Exception as e:
+            logger.info("Couldn't login user using username and password: %s" % e)
+
+    if not login_via_pw and not login_via_session:
+        raise Exception("Couldn't login user with either password or session")
+      
 st.header("IG BlackMarket")
-name = st.text_input("Enter Username:")
-password = st.text_input("Enter Password:", type="password")
+USERNAME = st.text_input("Enter Username:")
+PASSWORD = st.text_input("Enter Password:", type="password")
 Keyy = st.text_input("Set API-Key:")
 Go = st.button('Start')
 if Go:
-  cl = Client()
-  cl.login(name, password)
+  try:cl.load_settings("session.json");
+  except:
+    cl.login(USERNAME, PASSWORD)
+    cl.dump_settings("session.json")
+  login_user()
   openai.api_key = Keyy
   find_value = 20
   def generate_response(prompt):
