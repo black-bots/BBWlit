a0d08e8cf0a66f2a9f044f3726f30c511a322e11
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-08 15:10:50 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -34,9 +34,9 @@ st.markdown("""
 	margin-left: auto;
 	margin-right: auto;
     }
-    footer {
-    	display:none;
-    }
+    #footer {
+    #	display:none;
+    #}
     .st-emotion-cache-1v0mbdj {
 	text-align: center;
 	display: block;
