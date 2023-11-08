77e2798ce820c39258eef382d52aaf2d3f9a81cf
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-08 14:24:37 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -8,7 +8,7 @@ from PIL import Image
 image = Image.open('static/p1.png')
 image1 = Image.open('static/p1.jpg')
 st.image(image,use_column_width=True)
-st.markdown('-:green[Supremely Refined]')
+st.markdown("""---""")
 
 def paginator(label, items, items_per_page=10, on_sidebar=False):
     if on_sidebar:
