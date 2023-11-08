e18c53ba237a5afc728502f9a5921bf6754825be
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-08 14:05:15 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -56,7 +56,9 @@ st.markdown("""
 with st.expander("Pierre Polis",expanded=False):
 	st.write("""
 	:blue[Engagement & Fashion Jewelry from Pierre Polie]
+ 
 	:green[What is the perfect way to dress up any outfit or add your own flare? Accessorizing with jewelry, of course! 
+ 
 	From sparkling diamond necklaces and fashionable layered bracelets to statement earrings to sophisticated but trendy stackable rings, 
 	Pierre Polie has the perfect jewelry piece to accentuate your wardrobe, your lifestyle and your budget. 
 	Who says men can't dress up their wardrobe too?! Find sophisticated and handsome jewelry for men such as necklaces and chains. 
