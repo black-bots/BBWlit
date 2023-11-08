7474f5057bfcbe9b32758e7232505e3224a4097b
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-08 02:35:30 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -11,75 +11,73 @@ st.image(image,use_column_width=True)
 
 st.markdown('-:green[Supremely Refined]')
 featured = st.expander("Todays Featured Piece", expanded=True)
+def paginator(label, items, items_per_page=10, on_sidebar=True):
+    """Lets the user paginate a set of items.
+    Parameters
+    ----------
+    label : str
+	The label to display over the pagination widget.
+    items : Iterator[Any]
+	The items to display in the paginator.
+    items_per_page: int
+	The number of items to display per page.
+    on_sidebar: bool
+	Whether to display the paginator widget on the sidebar.
+	
+    Returns
+    -------
+    Iterator[Tuple[int, Any]]
+	An iterator over *only the items on that page*, including
+	the item's index.
+    Example
+    -------
+    This shows how to display a few pages of fruit.
+    >>> fruit_list = [
+    ...     'Kiwifruit', 'Honeydew', 'Cherry', 'Honeyberry', 'Pear',
+    ...     'Apple', 'Nectarine', 'Soursop', 'Pineapple', 'Satsuma',
+    ...     'Fig', 'Huckleberry', 'Coconut', 'Plantain', 'Jujube',
+    ...     'Guava', 'Clementine', 'Grape', 'Tayberry', 'Salak',
+    ...     'Raspberry', 'Loquat', 'Nance', 'Peach', 'Akee'
+    ... ]
+    ...
+    ... for i, fruit in paginator("Select a fruit page", fruit_list):
+    ...     st.write('%s. **%s**' % (i, fruit))
+    """
 
+    # Figure out where to display the paginator
+    if on_sidebar:
+	location = st.sidebar.empty()
+    else:
+	location = st.empty()
+
+    # Display a pagination selectbox in the specified location.
+    items = list(items)
+    n_pages = len(items)
+    n_pages = (len(items) - 1) // items_per_page + 1
+    page_format_func = lambda i: "Page %s" % i
+    page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
+
+    # Iterate over the items in the page to let the user display them.
+    min_index = page_number * items_per_page
+    max_index = min_index + items_per_page
+    import itertools
+    return itertools.islice(enumerate(items), min_index, max_index)
+
+def demonstrate_paginator():
+    fruit_list = [
+	'Kiwifruit', 'Honeydew', 'Cherry', 'Honeyberry', 'Pear',
+	'Apple', 'Nectarine', 'Soursop', 'Pineapple', 'Satsuma',
+	'Fig', 'Huckleberry', 'Coconut', 'Plantain', 'Jujube',
+	'Guava', 'Clementine', 'Grape', 'Tayberry', 'Salak',
+	'Raspberry', 'Loquat', 'Nance', 'Peach', 'Akee'
+    ]
+    for i, fruit in paginator("Select a fruit page", fruit_list):
+	st.write('%s. **%s**' % (i, fruit))
+
+
+if __name__ == '__main__':
+    demonstrate_paginator()
 with featured:
-
-	def paginator(label, items, items_per_page=10, on_sidebar=True):
-	    """Lets the user paginate a set of items.
-	    Parameters
-	    ----------
-	    label : str
-	        The label to display over the pagination widget.
-	    items : Iterator[Any]
-	        The items to display in the paginator.
-	    items_per_page: int
-	        The number of items to display per page.
-	    on_sidebar: bool
-	        Whether to display the paginator widget on the sidebar.
-	        
-	    Returns
-	    -------
-	    Iterator[Tuple[int, Any]]
-	        An iterator over *only the items on that page*, including
-	        the item's index.
-	    Example
-	    -------
-	    This shows how to display a few pages of fruit.
-	    >>> fruit_list = [
-	    ...     'Kiwifruit', 'Honeydew', 'Cherry', 'Honeyberry', 'Pear',
-	    ...     'Apple', 'Nectarine', 'Soursop', 'Pineapple', 'Satsuma',
-	    ...     'Fig', 'Huckleberry', 'Coconut', 'Plantain', 'Jujube',
-	    ...     'Guava', 'Clementine', 'Grape', 'Tayberry', 'Salak',
-	    ...     'Raspberry', 'Loquat', 'Nance', 'Peach', 'Akee'
-	    ... ]
-	    ...
-	    ... for i, fruit in paginator("Select a fruit page", fruit_list):
-	    ...     st.write('%s. **%s**' % (i, fruit))
-	    """
-	
-	    # Figure out where to display the paginator
-	    if on_sidebar:
-	        location = st.sidebar.empty()
-	    else:
-	        location = st.empty()
-	
-	    # Display a pagination selectbox in the specified location.
-	    items = list(items)
-	    n_pages = len(items)
-	    n_pages = (len(items) - 1) // items_per_page + 1
-	    page_format_func = lambda i: "Page %s" % i
-	    page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
-	
-	    # Iterate over the items in the page to let the user display them.
-	    min_index = page_number * items_per_page
-	    max_index = min_index + items_per_page
-	    import itertools
-	    return itertools.islice(enumerate(items), min_index, max_index)
-	
-	def demonstrate_paginator():
-	    fruit_list = [
-	        'Kiwifruit', 'Honeydew', 'Cherry', 'Honeyberry', 'Pear',
-	        'Apple', 'Nectarine', 'Soursop', 'Pineapple', 'Satsuma',
-	        'Fig', 'Huckleberry', 'Coconut', 'Plantain', 'Jujube',
-	        'Guava', 'Clementine', 'Grape', 'Tayberry', 'Salak',
-	        'Raspberry', 'Loquat', 'Nance', 'Peach', 'Akee'
-	    ]
-	    for i, fruit in paginator("Select a fruit page", fruit_list):
-	        st.write('%s. **%s**' % (i, fruit))
-	
-	
-	#if __name__ == '__main__':
-	#    demonstrate_paginator()	
 	sunset_imgs = [
 	    'static/p1.jpg',
 	    'https://static.wixstatic.com/media/0dfae7_8ebd2fd403514153a649d4fc899838e9~mv2.jpg',
@@ -97,7 +95,7 @@ with featured:
 	    'https://unsplash.com/photos/kOqBCFsGTs8/download?force=true',
 	    'https://unsplash.com/photos/8DMuvdp-vso/download?force=true'
 	]
-	image_iterator = demonstrate_paginator("Select a sunset page", sunset_imgs)
+	image_iterator = paginator("Select a sunset page", sunset_imgs)
 	indices_on_page, images_on_page = map(list, zip(*image_iterator))
 	st.image(images_on_page, width=100, caption=indices_on_page)
 def my_widget2(key):
