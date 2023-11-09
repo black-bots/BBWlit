b590d50be52c6ea9438ea049cfd9d0feb4cb6bbc
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-09 17:42:03 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -367,10 +367,124 @@ with col1:
 
 
 with col2:
-   st.write("Set2")
-   st.text("RandomName")
-   st.image('https://static.wixstatic.com/media/0dfae7_13f6f0f1de97447db6c8b31809dfd4d6~mv2.jpg', width=175)
-   st.write("[View More](https://link-center.net/674202/free-mario-bros-movie)")
+	st.write("Rings")
+	st.text("Bridal Set")
+	components.html(
+	    """
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	* {box-sizing: border-box;}
+	body {font-family: Verdana, sans-serif;}
+	.mySlides {display: none;}
+	img {vertical-align: middle;}
+	
+	/* Slideshow container */
+	.slideshow-container {
+	  max-width: 1000px;
+	  position: relative;
+	  margin: auto;
+	}
+	
+	/* Caption text */
+	.text {
+	  color: #f2f2f2;
+	  font-size: 15px;
+	  padding: 8px 12px;
+	  position: absolute;
+	  bottom: 8px;
+	  width: 100%;
+	  text-align: center;
+	}
+	
+	/* Number text (1/3 etc) */
+	.numbertext {
+	  color: #f2f2f2;
+	  font-size: 12px;
+	  padding: 8px 12px;
+	  position: absolute;
+	  top: 0;
+	}
+	/* The dots/bullets/indicators */
+	.dot {
+	  height: 6px;
+	  width: 6px;
+	  margin: 0 2px;
+	  background-color: #bbb;
+	  border-radius: 50%;
+	  display: inline-block;
+	  transition: background-color 0.6s ease;
+	}		
+	.active {
+	  background-color: #717171;
+	}
+	
+	/* Fading animation */
+	.fade {
+	  animation-name: fade;
+	  animation-duration: 1.5s;
+	}
+	
+	@keyframes fade {
+	  from {opacity: .4} 
+	  to {opacity: 1}
+	}
+	
+	/* On smaller screens, decrease text size */
+	@media only screen and (max-width: 300px) {
+	  .text {font-size: 11px}
+	}
+	</style>
+	</head>
+	<body>
+		
+	<div class="slideshow-container">
+	
+	<div class="mySlides fade">
+	  <img src="https://static.wixstatic.com/media/0dfae7_7158cf6a92c041dfaab52c6840092067~mv2.png" style="width:100%">
+	</div>
+	
+	<div class="mySlides fade">
+	  <img src="https://static.wixstatic.com/media/0dfae7_67c2f9eb5c61465ca54def74b745e1f2~mv2.jpg" style="width:100%">
+	</div>
+	
+	</div>
+	<br>
+	<div style="text-align:center">
+	  <span class="dot"></span> 
+	  <span class="dot"></span> 
+	</div>	
+	<script>
+	let slideIndex = 0;
+	showSlides();
+	
+	function showSlides() {
+	  let i;
+	  let slides = document.getElementsByClassName("mySlides");
+	  let dots = document.getElementsByClassName("dot");
+	  for (i = 0; i < slides.length; i++) {
+	    slides[i].style.display = "none";  
+	  }
+	  slideIndex++;
+	  if (slideIndex > slides.length) {slideIndex = 1}    
+	  for (i = 0; i < dots.length; i++) {
+	    dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";  
+	  dots[slideIndex-1].className += " active";
+	  setTimeout(showSlides, 2000); // Change image every 2 seconds
+	}
+	</script>
+	
+	</body>
+	</html> 
+	
+	    """,
+	    height=301,
+	)
+	st.write("[View More](https://link-center.net/674202/free-mario-bros-movie)")
 
 
 with col3:
