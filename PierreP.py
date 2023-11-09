753fbed2f4de26f01867cd6fa0d1eec4bf761a4a
black-bots <125762717+black-bots@users.noreply.github.com>
2023-11-09 01:37:13 -0500

    Update PierreP.py

--- PierreP.py
+++ PierreP.py
@@ -183,9 +183,9 @@ products = {
     "Price": [4, 12, 10],
     "Stocks": [100, 50, 60],
     "Details": [
-        "https://i.imgur.com/fH2LHvo.png",
-        "https://i.imgur.com/bvHZX5j.png",
-        "https://i.imgur.com/D7xDwT9.png"
+        "static/p1.jpg",
+        "static/p1.png",
+        "https://static.wixstatic.com/media/0dfae7_50700c2ac4a1434c9149ecd7d16f2f73~mv2.jpg"
     ]
 }
 
@@ -193,8 +193,6 @@ df = pd.DataFrame(products)
 
 ob = GridOptionsBuilder.from_dataframe(df)
 
-# 1. Add an image next to the value under the Name column.
-#    Also add a link on the label.
 image_cr = JsCode("""
     function(params) {
         var element = document.createElement("span");
@@ -202,13 +200,11 @@ image_cr = JsCode("""
         var imageElement = document.createElement("img");
         var anchorElement = document.createElement("a");
 
-        // Update the image element. Use the value from Details column.
         imageElement.src = params.data.Details;
         imageElement.width="80";
         imageElement.height="80";
         element.appendChild(imageElement);
 
-        // Add a link to the Name value. The link is from the Details column.
         anchorElement.href = params.data.Details;
         anchorElement.target = "_blank";
         anchorElement.innerHTML = params.value;
@@ -218,15 +214,12 @@ image_cr = JsCode("""
     }""")
 ob.configure_column('Name', cellRenderer=image_cr)
 
-# 2. Configure the column Details. Add a link to inner value.
 image_url = JsCode("""
     function(params) {
         return `<a href=${params.value} target="_blank">${params.value}</a>`
     }""")
 ob.configure_column("Details", cellRenderer=image_url)
 
-# 2.1. Style the cell, if stocks is below 60, increase font-size
-#      and color it red - as a warning to supply department.
 low_supply = JsCode("""
     function(params) {
         if (params.value < 60) {
@@ -238,16 +231,12 @@ low_supply = JsCode("""
     }""")
 ob.configure_column("Stocks", cellStyle=low_supply)
 
-# 3. Update selection.
 ob.configure_selection(selection_mode="multiple", use_checkbox=True)
 
-# 4. Update row height, to make the image look clearer.
 ob.configure_grid_options(rowHeight=100)
 
-# 5. Hide the Details column. The product name has a link already.
 ob.configure_column("Details", hide=True)
 
-# 6. Build the options.
 grid_options = ob.build()
 
 st.markdown('#### Streamlit-AgGrid')
