23d0b385d203784125aacbf26f6c6179537009e6
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-28 16:45:57 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -37,6 +37,19 @@ def get_driver():
         ),
         options=options,
     )
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
 driver = get_driver()
 
@@ -72,7 +85,9 @@ with tab1:
                         for paragraph in paragraphs:
                             story += paragraph.text + "\n\n"
                         story = story.replace('<p>', '').replace('</p>', '')
-                        speech=BytesIO();speech_=gTTS(text=story,lang='en',slow=False);speech_.write_to_fp(speech);st.audio(speech)
+                        speech=BytesIO();speech_=gTTS(text=story,lang='en',slow=False);speech_.write_to_fp(speech);#st.audio(speech)
+                        st.write("# Auto-playing Audio!")
+                        autoplay_audio(speech)
                         st.write(story)
     
                         for group in groups:
