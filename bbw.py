096d6ed01faeb4e8a2da5439c83820e44891d2ec
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-28 07:51:25 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -220,12 +220,12 @@ if ok:
                         group_text = ""
                         for d_paragraph in group:
                             group_text += d_paragraph.text + "\n"
-			finished = False
+                        finished = False
 			while not finished:
-				res_box.markdown(f':blue[BlackButler: ]:green[*{group_text}*]')
-				finished = True
-				if finished is True:
-					break
+                            res_box.markdown(f':blue[BlackButler: ]:green[*{group_text}*]')
+                            finished = True
+                            if finished is True:
+                                break
                 else:
                     res_box.markdown(f':blue[Dao: ]:green[*No manga content found at the provided URL.*]')
             else:
