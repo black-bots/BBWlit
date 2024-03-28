95f7e423e139d184134f305d6dd324a5140a0537
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-28 08:00:08 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -76,7 +76,7 @@ if ok:
                 res_box.markdown(f':blue[Dao: ]:green[*Failed to fetch URL. Check your internet connection or the validity of the URL.*]')
         except Exception as e:
             res_box.markdown(f':blue[Dao: ]:green[*Error occurred: {e}*]')
-    st.code(group_text)
+    st.code(paragraphs)
     st.download_button('Download Text', group_text, key="847*")
     st.markdown("____________________________________________________________")
 
