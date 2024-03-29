7afb796b6b7de906917d0e90adcf87aa263b9963
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-29 14:00:40 -0400

    Update bbw.py

--- bbw.py
+++ bbw.py
@@ -18,6 +18,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
