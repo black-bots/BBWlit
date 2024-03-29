d15fa71c37e5c7f6767baf64255e06845ec530d9
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-28 21:13:01 -0400

    Update gr.py

--- gr.py
+++ gr.py
@@ -67,42 +67,41 @@ while True:
     username_str = USERNAME
     hashtag_list = ["like", "follow", "follow", "chatgpt", "ai", "chatai"]
     
-    for hashtag in hashtag_list:
-      top_posts = cl.hashtag_medias_top(hashtag, find_value)
+    top_posts = cl.hashtag_medias_top(random.choice(hashtag), find_value)
     
-      for i in range(0, len(top_posts)):
-          first_comment = top_posts[i].dict()
+    for i in range(0, len(top_posts)):
+    first_comment = top_posts[i].dict()
     
-          post_id = first_comment['id']  
-          post_code = first_comment['code']
-          post_url = "https://instagram.com/reel/" + post_code
-          st.write(post_url)
+    post_id = first_comment['id']  
+    post_code = first_comment['code']
+    post_url = "https://instagram.com/reel/" + post_code
+    st.write(post_url)
     
-          media_id = cl.media_id(cl.media_pk_from_url(post_url))
-          
-          is_present = False
+    media_id = cl.media_id(cl.media_pk_from_url(post_url))
     
-          post_id = media_id
+    is_present = False
     
-          if  is_present == False:
-              st.write("New Post Found, Commenting..... \n")
-              try:
-                  ai_comm = "Write a nice generic Instagram Photo comment pertaining to a pleasant picture and ask for whomever to checkout your instagram page which is " + username_str + "."
-                  text = "Nice!"
-                  comment = cl.media_comment(post_id, str(text))
-                  st.write('Comment Left!')
-                  time.sleep(200)
-              except Exception as error:
-                  st.write(error)
-          else:
-              print("Post Already Found \n")
-      # Increase the time inorder to not get temporary ban
-      st.write("New Posts in 5 Seconds....")
-      time.sleep(60)
-  
+    post_id = media_id
+    
+    if  is_present == False:
+        st.write("New Post Found, Commenting..... \n")
+        try:
+            ai_comm = "Write a nice generic Instagram Photo comment pertaining to a pleasant picture and ask for whomever to checkout your instagram page which is " + username_str + "."
+            text = "Nice!"
+            comment = cl.media_comment(post_id, str(text))
+            st.write('Comment Left!')
+            time.sleep(200)
+        except Exception as error:
+            st.write(error)
+    else:
       print("Post Already Found \n")
-      # Increase the time inorder to not get temporary ban
-      st.write("New Posts in 5 Seconds....")
-      time.sleep(60)
-  
-      
+    # Increase the time inorder to not get temporary ban
+    st.write("New Posts in 5 Seconds....")
+    time.sleep(60)
+    
+    print("Post Already Found \n")
+    # Increase the time inorder to not get temporary ban
+    st.write("New Posts in 5 Seconds....")
+    time.sleep(60)
+    
+
