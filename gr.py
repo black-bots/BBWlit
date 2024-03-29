7530b694e2fe71f6ed26e0e0fc3566a77192a604
black-bots <125762717+black-bots@users.noreply.github.com>
2024-03-28 23:17:30 -0400

    Update gr.py

--- gr.py
+++ gr.py
@@ -16,48 +16,8 @@ main_image = Image.open('static/-.ico')
 top_image = Image.open('static/4.png')
 _image = Image.open('static/3.png')
 
-cl = Client()
-cl.delay_range = [2, 6]
-def login_user():
-    cl = Client()
-    session = cl.load_settings("session.json")
-
-    login_via_session = False
-    login_via_pw = False
-
-    if session:
-        try:
-            cl.set_settings(session)
-            cl.login(USERNAME, PASSWORD)
-
-            # check if session is valid
-            try:
-                cl.get_timeline_feed()
-            except LoginRequired:
-                logger.info("Session is invalid, need to login via username and password")
-
-                old_session = cl.get_settings()
-
-                cl.set_settings({})
-                cl.set_uuids(old_session["uuids"])
-
-                cl.login(USERNAME, PASSWORD)
-            login_via_session = True
-        except Exception as e:
-            logger.info("Couldn't login user using session information: %s" % e)
-
-    if not login_via_session:
-        try:
-            logger.info("Attempting to login via username and password. username: %s" % USERNAME)
-            if cl.login(USERNAME, PASSWORD):
-                login_via_pw = True
-        except Exception as e:
-            logger.info("Couldn't login user using username and password: %s" % e)
-
-    if not login_via_pw and not login_via_session:
-        raise Exception("Couldn't login user with either password or session")
-
 st.image(bottom_image,use_column_width='auto')
+
 USERNAME = st.text_input("Enter Username:")
 PASSWORD = st.text_input("Enter Password:", type="password")
 with st.sidebar:
@@ -122,6 +82,47 @@ with st.sidebar:
 
 Go = st.button('Start')
 if Go:
+	cl = Client()
+	cl.delay_range = [2, 6]
+	def login_user():
+	    cl = Client()
+	    session = cl.load_settings("session.json")
+	
+	    login_via_session = False
+	    login_via_pw = False
+	
+	    if session:
+	        try:
+	            cl.set_settings(session)
+	            cl.login(USERNAME, PASSWORD)
+	
+	            # check if session is valid
+	            try:
+	                cl.get_timeline_feed()
+	            except LoginRequired:
+	                logger.info("Session is invalid, need to login via username and password")
+	
+	                old_session = cl.get_settings()
+	
+	                cl.set_settings({})
+	                cl.set_uuids(old_session["uuids"])
+	
+	                cl.login(USERNAME, PASSWORD)
+	            login_via_session = True
+	        except Exception as e:
+	            logger.info("Couldn't login user using session information: %s" % e)
+	
+	    if not login_via_session:
+	        try:
+	            logger.info("Attempting to login via username and password. username: %s" % USERNAME)
+	            if cl.login(USERNAME, PASSWORD):
+	                login_via_pw = True
+	        except Exception as e:
+	            logger.info("Couldn't login user using username and password: %s" % e)
+	
+	    if not login_via_pw and not login_via_session:
+	        raise Exception("Couldn't login user with either password or session")
+
 	try:
 		cl.load_settings("session.json")
 	except:
