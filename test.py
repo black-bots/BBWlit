8a12391f4ad12d4ca8b0d8dd44469f5c825b4799
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-12 15:17:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,54 @@
+# ┌──────────────────────────────────┐
+# │ BlackGram - Manga Dōjutsu v1.22  │
+# ├──────────────────────────────────┤
+# │ Copyright © 2024 BlackBots.net   │
+# │ (https://BlackBots.net)          │
+# ├──────────────────────────────────┤
+# │ Developer: @Supreme.Ciento       │
+# ├──────────────────────────────────┤
+# │ Licensed under the MIT           │
+# │ (https://BlackBots.net/license)  │
+# └──────────────────────────────────┘
+#            マンガアプリ
+
+# 010011010110000101101110011001110110000101000100011011110110101001110101011101000111001101110101
+ 
+# 77971101039768111106117116115117 
+                                                                      
+#    x*8888x.:*8888: -"888:                                                                     
+#   X   48888X `8888H  8888                                                         
+#  X8x.  8888X  8888X  !888>               x@88k u@88c.                  
+#  X8888 X8888  88888   "*8%-    us888u.  ^"8888""8888"  .ue888Nc..    us888u.                  
+#  '*888!X8888> X8888  xH8>   .@88 "8888"   8888  888R  d88E`"888E` .@88 "8888"                 
+#    `?8 `8888  X888X X888>   9888  9888    8888  888R  888E  888E  9888  9888                  
+#    -^  '888"  X888  8888>   9888  9888    8888  888R  888E  888E  9888  9888                  
+#     dx '88~x. !88~  8888>   9888  9888    8888  888R  888E  888E  9888  9888                  
+#   .8888Xf.888x:!    X888X.: 9888  9888   "*88*" 8888" 888& .888E  9888  9888                  
+#  :""888":~"888"     `888*"  "888*""888"    ""   'Y"   *888" 888&  "888*""888"                 
+#      "~'    "~        ""     ^Y"   ^Y'                 `"   "888E  ^Y"   ^Y'                  
+#                                                       .dWi   `88E                             
+#                                                       4888~  J8%                              
+#                                                        ^"===*"`                               
+#         ....                                                          .x+=:.                  
+#     .xH888888Hx.                     .                               z`    ^%                 
+#   .H8888888888888:     *^^^^^^^*    888>                    .88             <k
+#   888*"""?""*88888X    ...ue888b    "8P    .@88k  z88u     :888ooo    .@8Ned8"  .@88k  z88u   
+#  'f     d8x.   ^%88k   888R Y888r    .    ~"8888  8888   -*8888888  .@^%8888"  ~"8888  8888   
+#  '>    <88888X   '?8   888R I888>  u888u.   8888  888R     8888    x88:  `)8b.   8888  888R   
+#   `:..:`888888>    8>  888R I888> `'888E    8888  888R     8888    8888N=*8888   8888  888R   
+#          `"*88     X   888R I888>   888E    8888  888R     8888     %8"    R88   8888  888R   
+#     .xHHhx.."      !  u8888cJ888    888E    8888 ,888B .  .8888Lu=   @8Wou 9%    8888 ,888B . 
+#    X88888888hx. ..!    "*888*P"     888E   "8888Y 8888"   ^%888*   .888888P`    "8888Y 8888"  
+#   !   "*888888888"       'Y"        888E    `Y"   'YP       'Y"    `   ^"F       `Y"   'YP    
+#          ^"***"`                    888E                                                      
+#                                     888E                                                      
+#                                     888P                                                      
+#                                   .J88" "                                                     
 import os
 import base64
 import time
 import tempfile
+import io
 from io import BytesIO
 
 import re
@@ -16,6 +63,7 @@ import numpy as np  # Image Processing
 from easyocr import Reader
 import streamlit as st
 import streamlit_nested_layout
+import streamlit.components.v1 as components
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -26,6 +74,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 from bs4 import BeautifulSoup
+import webbrowser
+
 
 history = []
 
@@ -37,6 +87,7 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded"
 )
+
 st.markdown("""
     <style>
         <br><hr><center>
@@ -64,9 +115,15 @@ st.markdown("""
         #MainMenu{visibility: hidden;}
         footer{visibility: hidden;}
         .css-14xtw13 e8zbici0{visibility: hidden;}
+        .css-m70y {display:none}
+        .st-emotion-cache-zq5wmm.ezrtsby0{visibility: hidden;}
+        .st-emotion-cache-zq5wmm.ezrtsby0{display:none}
+        .styles_terminalButton__JBj5T{visibility: hidden;}
+        .styles_terminalButton__JBj5T{display:none}
+        .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N{visibility: hidden;}
+        .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N{display:none}
     </style>
 """, unsafe_allow_html=True)
-
 
 options = Options()
 options.add_argument("--disable-gpu")
@@ -115,20 +172,6 @@ with st.sidebar:
     outer_cols = st.columns([1, 1])
     with col1:
         with st.expander("Text Based"):
-            st.caption("Example: https://daotranslate.us/solo-leveling-ragnarok-chapter-1/")
-            with st.expander("Latest Releases"):
-                resp = requests.get("https://daotranslate.us/series/?status=&type=&order=update")
-                if resp.status_code == 200:
-                    soup = BeautifulSoup(resp.text, 'html.parser')
-                    manga_list_div = soup.find("div", {"class": "listupd"})
-                    if manga_list_div:
-                        titles = manga_list_div.find_all("div", {"class": "mdthumb"})
-                        for title in titles:
-                            title_url = title.a["href"]
-                            title_name = title_url.split("series/")[1]
-                            title_name = title_name.replace('/', '')
-                            title_name = title_name.title()
-                            st.write(f"Title: :green[{title_name}]  \nURL: {title_url}\n")
             with st.expander("Search.."):
                 search_variable = st.text_input(":orange[Title:]", placeholder="Martial Peak", key='search', help="Enter a title here to search for")
                 search_url = f"https://daotranslate.us/?s={search_variable}"
@@ -143,9 +186,29 @@ with st.sidebar:
                             title_name = title_url.split("series/")[1]
                             title_name = title_name.replace('/', '')
                             title_name = title_name.title()
-                            st.write(f"Title: :green[{title_name}]  \nURL: {title_url}\n")
+                            img_url = title.img["src"]
+                            st.image(img_url, caption=title_name)
                             ch = f"https://daotranslate.us/{title_name}-chapter-1/"
-                            st.write(f"CH 01: {ch}")
+                            st.write(f"{ch}")
+            with st.expander("Latest Releases"):
+                resp = requests.get("https://daotranslate.us/series/?status=&type=&order=update")
+                if resp.status_code == 200:
+                    soup = BeautifulSoup(resp.text, 'html.parser')
+                    image_elements = soup.find_all('div', {"class": "mdthumb"})
+                    for image_element in image_elements:
+                        img_src = image_element.find('img')['src']
+                    manga_list_div = soup.find("div", {"class": "listupd"})
+                    if manga_list_div:
+                        titles = manga_list_div.find_all("div", {"class": "mdthumb"})
+                        for title in titles:
+                            title_url = title.a["href"]
+                            title_name = title_url.split("series/")[1]
+                            title_name = title_name.replace('/', '')
+                            title_name = title_name.title()
+                            img_url = title.img["src"]
+                            st.image(img_url, caption=title_name)
+                            ih = f"https://daotranslate.us/{title_name}-chapter-1/"
+                            st.write(f":green[{ih}]")
     with col2:
         with st.expander("Image Based"):
             st.caption("Example: https://manhuaaz.com/manga/monster-pet-evolution/chapter-1/")
@@ -159,7 +222,8 @@ with st.sidebar:
                         href = link.get("href")
                         manga_name = href.split("https://manhuaaz.com/manga/")[1]
                         ch = f"{href}/chapter-1/"
-                        st.write(f"Title: :green[{manga_name}]  \nCH 01: {ch}\n")
+                        st.caption(manga_name)
+                        st.write(f"{ch}")
                         
             with st.expander("Search.."):
                 search_variable = st.text_input(":orange[Title:]", placeholder="Martial Peak", key='search2', help="Enter a title here to search for")
@@ -173,11 +237,14 @@ with st.sidebar:
                         title_name = tab_thumb.find("a")['title']
                         title_url = tab_thumb.find("a")['href']
                         ch = f"{title_url}chapter-1/"
-                        st.write(f"Title: :green[{title_name}]  \nCH 01: {ch}\n")
-    url = st.text_input(":orange[CH. Url:]", placeholder="https://daotranslate.us/solo-leveling-ragnarok-chapter-1/", key='input', help="Enter manga chapter URL here")
+                        st.write(f"{ch}")
+    url = st.text_input(":orange[Enter URL:]", value=ih, placeholder="https://daotranslate.us/solo-leveling-ragnarok-chapter-1/", key='input', help="Enter manga chapter URL here")
     ok = st.button("📚Read", help="Read", key='123', use_container_width=False)
+    st.header("Google Play Store")
+    st.caption("Download from: https://play.google.com/store/apps/details?id=com.blackbots.blackdao")
     st.header("Official PC Version")
     st.caption("Download from: https://blackbots.gumroad.com/l/manga")
+
     st.caption("Join Our Discord: https://discord.gg/HcVPaSpF")
 
 tab1,tab2=st.tabs(['Text Based','Image Based'])
@@ -234,6 +301,7 @@ with tab1:
                                 new_file = speedup(audio,1.2,150)
                                 new_file.export("file.mp3", format="mp3")
                                 autoplay_audio("file.mp3")
+                                #st.download_button("file.mp3")
 
                             for group in groups:
                                 group_text = ""
@@ -327,35 +395,11 @@ def is_image_link(link):
             return True
     return False
 
-def get_image_links(url):
-    try:
-        driver.get(url)
-    except WebDriverException as ex:
-        if driver.current_url == url:
-            print(f"Error loading URL: {ex}")
-        else:
-            print(f"Error loading URL: {ex}")
-        return []
-
-    image_links = []
-
-    img_elements = driver.find_elements(By.CSS_SELECTOR, 'img')
-
-    for img_element in img_elements:
-        img_src = img_element.get_attribute('src')
-
-        if img_src and is_image_link(img_src):
-            image_links.append(img_src)
-
-    driver.quit()
-    print("Image links:", image_links)  # Print extracted image links for debugging
-    return image_links
-
 if 'image_links' not in st.session_state:
     st.session_state.image_links = []
 if 'current_image_index' not in st.session_state:
     st.session_state.current_image_index = 0
-
+    
 @st.cache_resource
 def load_model() -> Reader:
     return ocr.Reader(["en"], model_storage_directory=".")
@@ -396,7 +440,7 @@ with tab2:
                     transcribe_to_audio([current_image_link])
                     
         except Exception as e:
-            print(f"Error: {e}")  # Print any error for debugging
+            st.write(f"Error: {e}")
  
-st.markdown("<br><hr><center>© Cloud Bots™ BlackBots. All rights reserved. by <a href='mailto:admin@blackbots.net?subject=BBWeb App!&body=Please specify the issue you are facing with the app.'><strong>BlackBots</strong></a></center><hr>", unsafe_allow_html=True)
+st.markdown("<br><hr><center>© Cloud Bots™ BlackBots. All rights reserved.  <a href='mailto:admin@blackbots.net?subject=MangaDojutsu!&body=Please specify the issue you are facing with the app.'><strong>BlackBots.net</strong></a></center><hr>", unsafe_allow_html=True)
 st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
