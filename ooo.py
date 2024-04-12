d3132dda0415c9453895c4525caf677c266c5fff
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-12 16:08:51 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -170,41 +170,40 @@ with st.sidebar:
     
     col1, col2 = st.columns(2)
     outer_cols = st.columns([1, 1])
+    search_variable = st.text_input(":orange[Search:]", placeholder="", key='search', help="Enter a title here to search for")
     with col1:
-        with st.expander("Text Based"):
-            search_variable = st.text_input(":orange[Search:]", placeholder="", key='search', help="Enter a title here to search for")
-            if search_variable:
-                with st.expander("Results.."):
-                    search_url = f"https://daotranslate.us/?s={search_variable}"
-                    resp = requests.get(search_url)
-                    if resp.status_code == 200:
-                        soup = BeautifulSoup(resp.text, 'html.parser')
-                        search_result_div = soup.find("div", {"class": "listupd"})
-                        if search_result_div:
-                            titless = search_result_div.find_all("div", {"class": "mdthumb"})
-                            for title in titless:
-                                title_url = title.a["href"]
-                                title_name = title_url.split("series/")[1]
-                                title_name = title_name.replace('/', '')
-                                title_name = title_name.title()
-                                img_url = title.img["src"]
-                                st.image(img_url, caption=title_name)
-                                ch = f"https://daotranslate.us/{title_name}-chapter-1/"
-                                st.write(f"{ch}")
-            with st.expander("Latest Releases"):
-                resp = requests.get("https://daotranslate.us/")
+        if search_variable:
+            with st.expander("Results.."):
+                search_url = f"https://daotranslate.us/?s={search_variable}"
+                resp = requests.get(search_url)
                 if resp.status_code == 200:
                     soup = BeautifulSoup(resp.text, 'html.parser')
-                    manga_list_div = soup.find("div", {"class": "listupd"})
-                    if manga_list_div:
-                        titles = manga_list_div.find_all("div", {"class": "mdthumb"})
-                        for title in titles:
+                    search_result_div = soup.find("div", {"class": "listupd"})
+                    if search_result_div:
+                        titless = search_result_div.find_all("div", {"class": "mdthumb"})
+                        for title in titless:
                             title_url = title.a["href"]
-                            title_name = title_url.split("series/")[1].replace('/', '').title()
-                            ih = f"https://daotranslate.us/{title_name}-chapter-1/"
-                            st.write(f"[{title_name} - Chapter 1]({ih})")
+                            title_name = title_url.split("series/")[1]
+                            title_name = title_name.replace('/', '')
+                            title_name = title_name.title()
                             img_url = title.img["src"]
-                            st.image(img_url, caption=title_name)  
+                            st.image(img_url, caption=title_name)
+                            ch = f"https://daotranslate.us/{title_name}-chapter-1/"
+                            st.write(f"{ch}")
+        with st.expander("Latest Releases"):
+            resp = requests.get("https://daotranslate.us/")
+            if resp.status_code == 200:
+                soup = BeautifulSoup(resp.text, 'html.parser')
+                manga_list_div = soup.find("div", {"class": "listupd"})
+                if manga_list_div:
+                    titles = manga_list_div.find_all("div", {"class": "mdthumb"})
+                    for title in titles:
+                        title_url = title.a["href"]
+                        title_name = title_url.split("series/")[1].replace('/', '').title()
+                        ih = f"https://daotranslate.us/{title_name}-chapter-1/"
+                        st.write(f"[{title_name} - Chapter 1]({ih})")
+                        img_url = title.img["src"]
+                        st.image(img_url, caption=title_name)  
     with col2:
         with st.expander("Image Based"):
             st.caption("Example: https://manhuaaz.com/manga/monster-pet-evolution/chapter-1/")
