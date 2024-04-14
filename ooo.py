c5a4e599a4940e9af931fec3f648dc4735f59f15
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-14 18:50:37 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -284,7 +284,7 @@ with st.sidebar:
                     ih = f"https://daotranslate.us/{title_name}-chapter-1/"
                     st.write(f"[{title_name}]({ih})")
                     img_url = title.img["src"]
-                    st.image(img_url, caption=ih)
+                    st.image(img_url, caption=ih, width=100)
                     button_key = title_name
                     if ih:
                         lisp = st.button("Play", key=button_key+ih)
