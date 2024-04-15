1319c7b4a361e4149112e0940f71a104db975990
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-15 01:08:20 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -455,7 +455,6 @@ with st.sidebar:
         st.caption("- View Image Based Links with the Image Based Tab")
 
 url = st.text_input(":orange[Enter Link:]", value='', placeholder="https://daotranslate.us/solo-leveling-ragnarok-chapter-1/", key=generate_unique_key(), help="Enter manga chapter URL here")
-ih = url
 ok = st.button("📚Read", help="Read", key=generate_unique_key(), use_container_width=False)
 tab1,tab2=st.tabs(['Text Based','Image Based'])
 with tab1:    
