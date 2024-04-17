b1b3627af443b1bcd627c62ff4394b43eb9f4d0d
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-17 16:31:02 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -474,6 +474,9 @@ with st.sidebar:
 xx = st.text_input(":orange[Enter Link:]", value='', placeholder="https://daotranslate.us/solo-leveling-ragnarok-chapter-1/", key='readfield', help="Enter manga chapter URL here")
 
 ok = st.button("📚Read", help="Read", key='readbutton', use_container_width=False)
+if st.button("Link", key=generate_unique_key()):
+    st.session_state.show_main_button = True
+    sx = ch
 if st.session_state.show_main_button:
     if st.button("Button on Main Window", generate_unique_key()):
         with st.spinner('Loading text & audio..'):
