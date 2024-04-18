1fa6931b1cebad13565d5e5273f7d4f07bb47590
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-18 00:26:27 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -67,7 +67,7 @@ from easyocr import Reader
 import streamlit as st
 import streamlit_nested_layout
 import streamlit.components.v1 as components
-from streamlit_extras.beta_expander import beta_expander
+import streamlit_extras
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
