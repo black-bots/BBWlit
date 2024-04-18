457140af7df4503fcf94414c918515c627c5f1bc
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-18 01:18:39 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -389,7 +389,7 @@ outer_cols = st.columns([1, 3])
 with col1:
     show_titles = st.button(":books: Random Titles", key=generate_unique_key())
     if show_titles:
-        with st.expander(':books:'):
+        with st.expander(''):
             resp = requests.get("https://daotranslate.us/?s=i")
             if resp.status_code == 200:
                 soup = BeautifulSoup(resp.text, 'html.parser')
@@ -404,6 +404,56 @@ with col1:
                         img_url = title.img["src"]
                         if img_url:
                             st.image(img_url, caption=ch, use_column_width='always')
+                        submed = st.button("Play:loud_sound:", key=generate_unique_key())  # Fix: Button for text-based content
+                        if submed:
+                            try:
+                                driver.quit()
+                            except:
+                                pass
+                            with st.spinner('Loading text & audio..'):
+                                driver = get_driver()
+                                driver.get(url)
+                        
+                                if not url:
+                                    res_box.markdown(f':blue[Dao: ]:green[*Enter a valid URL before running.*]')
+                                else:
+                                    try:
+                                        resp = requests.get(url)
+                                        if resp.status_code == 200:
+                                            soup = BeautifulSoup(resp.text, 'html.parser')
+                                            d = soup.find("div", {"class": "epcontent entry-content"})
+                                            if d:
+                                                all_text = ""
+                                                num_paragraphs = len(d.findAll("p"))
+                                                paragraphs = d.findAll("p")
+                                                desired_group_size = 1  # Set your desired group size here
+                                                num_groups = num_paragraphs // desired_group_size  # Calculate the number of groups based on desired group size
+                                                groups = [paragraphs[i:i + desired_group_size] for i in range(0, len(paragraphs), desired_group_size)]
+                                                story = ""
+                                                for paragraph in paragraphs:
+                                                    story += paragraph.text + "\n"
+                                                story = story.replace('<p>', '')
+                                                story = story.replace('"', '')
+                                                
+                                                with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as tmp_file:
+                                                    story = story.replace('"','')
+                                                    tts = gTTS(text=story, lang='en', slow=False)
+                                                    tts.save(tmp_file.name)                            
+                                                    audio = AudioSegment.from_mp3(tmp_file.name)
+                                                    new_file = speedup(audio,1.2,150)
+                                                    new_file.export("file.mp3", format="mp3")
+                                                    autoplay_audio("file.mp3")
+                                                    #st.download_button("file.mp3")
+                                            
+                                                driver.quit()
+                                            else:
+                                                st.write('')
+                                        else:
+                                            st.write(f':blue[Dao: ]:green[*Failed to fetch URL. Check your internet connection or the validity of the URL.*]')
+                                    except Exception as e:
+                                        st.write(f':blue[Dao: ]:green[*Error occurred: {e}*]')
+                                driver.quit()
+                        st.divider()
 
 with col2:
     image_titles = st.button(":frame_with_picture: Image", key=generate_unique_key())
