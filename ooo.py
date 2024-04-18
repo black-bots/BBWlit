4ec7a59bc40e6191859f893a653a4f00c28d5f32
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-17 23:32:29 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -285,6 +285,52 @@ def readit(url):
             st.write(f':blue[Dao: ]:green[*Error occurred: {e}*]')
     driver.quit()
 
+def playit(url):
+    driver = get_driver()
+    try:
+        driver.get(url)
+    except:
+        pass
+    if not url:
+        res_box.markdown(f':blue[Dao: ]:green[*Enter a valid URL before running.*]')
+    else:
+        try:
+            resp = requests.get(url)
+            if resp.status_code == 200:
+                soup = BeautifulSoup(resp.text, 'html.parser')
+                d = soup.find("div", {"class": "epcontent entry-content"})
+                if d:
+                    all_text = ""
+                    num_paragraphs = len(d.findAll("p"))
+                    paragraphs = d.findAll("p")
+                    desired_group_size = 1  # Set your desired group size here
+                    num_groups = num_paragraphs // desired_group_size  # Calculate the number of groups based on desired group size
+                    groups = [paragraphs[i:i + desired_group_size] for i in range(0, len(paragraphs), desired_group_size)]
+                    story = ""
+                    for paragraph in paragraphs:
+                        story += paragraph.text + "\n"
+                    story = story.replace('<p>', '')
+                    story = story.replace('"', '')
+                    
+                    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as tmp_file:
+                        story = story.replace('"','')
+                        tts = gTTS(text=story, lang='en', slow=False)
+                        tts.save(tmp_file.name)                            
+                        audio = AudioSegment.from_mp3(tmp_file.name)
+                        new_file = speedup(audio,1.2,150)
+                        new_file.export("file.mp3", format="mp3")
+                        autoplay_audio("file.mp3")
+                        #st.download_button("file.mp3")
+
+                    driver.quit()
+                else:
+                    st.write('')
+            else:
+                st.write(f':blue[Dao: ]:green[*Failed to fetch URL. Check your internet connection or the validity of the URL.*]')
+        except Exception as e:
+            st.write(f':blue[Dao: ]:green[*Error occurred: {e}*]')
+    driver.quit()
+
 history = []
 ih = ""
 icob = Image.open('static/-.ico')
@@ -403,7 +449,7 @@ with col1:
                     submed = st.button("Play:loud_sound:", key=generate_unique_key())
                     if submed:
                         with st.spinner('Loading text & audio..'):
-                            readit(ch)
+                            playit(ch)
                     st.divider()
 with col2:        
     with st.expander(":frame_with_picture: Image"):
@@ -452,7 +498,9 @@ with col3:
                                 st.image(img_url, caption=ih)
 
                         submitted = st.button("Play:loud_sound:", key=generate_unique_key())
-
+                        if submitted:
+                            with st.spinner('Loading text & audio..'):
+                                playit(ih)
                         st.divider()
                             
 st.image(main_image)
@@ -464,14 +512,7 @@ ok = st.button(":green_book: Read", help="Read", key='readbutton', use_container
     
 tab1,tab2=st.tabs(['Text Based','Image Based'])
 
-with tab1:                
-    try:
-        if submitted:
-            with st.spinner('Loading text & audio..'):
-                readit(ih)
-    except:
-        pass
-        
+with tab1:                  
     if "daotrans" in xx:
         if ok:
             with st.spinner('Loading text & audio..'):
