982c11fc99271035ec1949a5c112a2beda0727ad
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-21 13:20:27 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -494,33 +494,34 @@ with col1:
                     st.divider()
 
 with col2:
-    with st.expander(f":frame_with_picture: Comics(Images)"):
-        resp = requests.get("https://manhuatop.org/")
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        manga_items = soup.find_all("div", class_="item-thumb")
-    
-        for item in manga_items:
-            link = item.find("a")
-            img_tag = item.find("img")
-            if link and img_tag:
-                href = link.get("href")
-                img_url = img_tag.get("data-src")
+    with st.expander(f":frame_with_picture: Manga Updates"):
+        resp = requests.get("https://mangaplus.shueisha.co.jp/updates")
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            updated_titles = soup.find_all("div", class_="UpdatedTitle-module_title_2KlMr")
+        
+            for title in updated_titles:
+                link = title.find("a")
+                img_tag = title.find("img")
+                title_name = title.find("p", class_="UpdatedTitle-module_titleName_1QO_s").text.strip()
+                author = title.find("p", class_="UpdatedTitle-module_author_1ltec").text.strip()
                 
-                manga_name = link.get("title")
-                
-                original_string = href
-                obfuscated_text, mapping = obfuscate(original_string)
-                url = deobfuscate(obfuscated_text, mapping)
-                
-                st.write(f"[{manga_name}]({href})")
-                st.image(img_url, use_column_width='always')
-                txt = f"""
-                {obfuscated_text}
-                """
-                st.code(txt, language='java')
-                st.caption('Copy Code')
-                st.divider()
-
+                if link and img_tag:
+                    href = link.get("href")
+                    img_url = img_tag.get("data-src")
+                    
+                    original_string = href
+                    obfuscated_text, mapping = obfuscate(original_string)
+                    url = deobfuscate(obfuscated_text, mapping)
+                    
+                    st.write(f"[{title_name}]({href}) by {author}")
+                    st.image(img_url, use_column_width='always')
+                    txt = f"""
+                    {obfuscated_text}
+                    """
+                    st.code(txt, language='java')
+                    st.caption('Copy Code')
+                    st.divider()
 with col3:
     with st.expander(f":frame_with_picture: Image"):
         resp = requests.get("https://manhuaaz.com/")
