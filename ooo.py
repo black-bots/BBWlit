e39ff3c9c18af616b5d5b2d026f3920202ef7d46
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-26 17:39:55 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -557,15 +557,17 @@ async def main():
                     obfuscated_text, mapping = await obfuscate(original_string)
                     if img_url:
                         with col1:
-                            st.image(img_url, use_column_width='always')
+			    with st.expander(f"{category}"):
+                                st.image(img_url, use_column_width='always')
                     if ch:
                         txt = f"""
                         {obfuscated_text}
                         """
                         url = await deobfuscate(obfuscated_text, mapping)
                         with col1:
-                            st.code(txt, language='java')
-                            st.button('Read', on_click=readit, args=[url], key=generate_unique_key())
+                            with st.expander(f"{category}"):
+                                st.code(txt, language='java')
+                                st.button('Read', on_click=readit, args=[url], key=generate_unique_key())
                     st.divider()
 
     urls = {
