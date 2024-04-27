1bc0f16a28a38ca0c278f08d76cee4b2c57cb5d6
black-bots <125762717+black-bots@users.noreply.github.com>
2024-04-26 20:51:12 -0400

    Update ooo.py

--- ooo.py
+++ ooo.py
@@ -423,80 +423,75 @@ with st.sidebar:
         st.caption("- `Press Read`")
     st.button("Restart", on_click=update_value, key='keyy')
 
-async def search_function():
-    search_variable = st.text_input(":orange[Search:]", placeholder="Search..", key='search', help="Enter a title here to search for")
-    
-    if search_variable:
+search_variable = st.text_input(":orange[Search:]", placeholder="Search..", key='search', help="Enter a title here to search for")
+                            
+if search_variable:
+    if '"' in search_variable:
         search_variable = search_variable.replace('"', '')
-        search_variable = search_variable.replace('!', '')
-        search_variable = search_variable.replace('-', ' ')
-        search_variable = search_variable.replace('?', '')
-        with st.spinner('Searching..'):
-            with st.expander(":mag: Search"):
-                search_url_1 = f"https://daotranslate.net/?s={search_variable}"
-                resp_1 = await httpx.get(search_url_1)
-                search_url_2 = f"https://manhuaaz.com/?s={search_variable}&post_type=wp-manga&op=&author=&artist=&release=&adult="
-                resp_2 = await httpx.get(search_url_2)
+    with st.spinner('Searching..'):
+        with st.expander(":mag: Search"):
+            search_url_1 = f"https://daotranslate.us/?s={search_variable}"
+            resp_1 = httpx.get(search_url_1)
+            search_url_2 = f"https://manhuaaz.com/?s={search_variable}&post_type=wp-manga&op=&author=&artist=&release=&adult="
+            resp_2 = httpx.get(search_url_2)
+            
+            if resp_1.status_code == 200 and resp_2.status_code == 200:
+                soup_1 = BeautifulSoup(resp_1.text, 'html.parser')
+                soup_2 = BeautifulSoup(resp_2.text, 'html.parser')
                 
-                if resp_1.status_code == 200 and resp_2.status_code == 200:
-                    soup_1 = BeautifulSoup(resp_1.text, 'html.parser')
-                    soup_2 = BeautifulSoup(resp_2.text, 'html.parser')
-                    
-                    search_result_div_1 = soup_1.find("div", {"class": "listupd"})
-                    search_result_div_2 = soup_2.find_all("a", href=lambda href: href and href.startswith("https://manhuaaz.com/manga/"))
-                    
-                    if search_result_div_1 and search_result_div_2:
-                        titles = search_result_div_1.find_all("div", {"class": "mdthumb"})
-                        manga_links = iter(search_result_div_2)
-                        for title in titles:
-                            title_url = title.a["href"]
-                            title_name = title_url.split("series/")[1].replace('/', '').title()
-                            titlename = title_name.replace('-', ' ')
-                            ih = f"https://daotranslate.net/{title_name}-chapter-1/"
-                            with st.spinner('Searching..'):
-                                st.write(f"[{titlename}]({ih})")
-                                img_url = title.img["src"]
-                                original_string = ih
+                search_result_div_1 = soup_1.find("div", {"class": "listupd"})
+                search_result_div_2 = soup_2.find_all("a", href=lambda href: href and href.startswith("https://manhuaaz.com/manga/"))
+                
+                if search_result_div_1 and search_result_div_2:
+                    titles = search_result_div_1.find_all("div", {"class": "mdthumb"})
+                    manga_links = iter(search_result_div_2)
+                    for title in titles:
+                        title_url = title.a["href"]
+                        title_name = title_url.split("series/")[1].replace('/', '').title()
+                        titlename = title_name.replace('-', ' ')
+                        ih = f"https://daotranslate.us/{title_name}-chapter-1/"
+                        with st.spinner('Searching..'):
+                            st.write(f"[{titlename}]({ih})")
+                            img_url = title.img["src"]
+                            original_string = ih
+                            obfuscated_text, mapping = obfuscate(original_string)
+                            if img_url:
+                                st.image(img_url, use_column_width='always')
+                            if ih:
+                                txt = f"""
+                                {obfuscated_text}
+                                """
+                                url = deobfuscate(obfuscated_text, mapping)
+                                st.code(txt, language='java')
+                                st.button('Read', on_click=readit, args=[url], key=generate_unique_key())
+                            st.divider()
+                            
+                            # Display results from search_result_div_2
+                            try:
+                                link = next(manga_links)
+                                href = link.get("href")
+                                if "chapter" not in href:
+                                    cch = f"{href}chapter-1/"
+                                else:
+                                    cch = href
+                                manga_name=href.split('https://manhuaaz.com/manga/')[1]
+                               
+                                img_tag = link.find("img")
+                                original_string = cch
                                 obfuscated_text, mapping = obfuscate(original_string)
-                                if img_url:
-                                    st.image(img_url, use_column_width='always')
-                                if ih:
+                                if img_tag:
+                                    st.write(f"[{manga_name}]({cch})")
+                                    img_url = img_tag.get("data-src")
+                                    st.image(img_url, caption=obfuscated_text, use_column_width='always')
+                                    
                                     txt = f"""
                                     {obfuscated_text}
                                     """
-                                    url = deobfuscate(obfuscated_text, mapping)
                                     st.code(txt, language='java')
-                                    st.button('Read', on_click=readit, args=[url], key=generate_unique_key())
-                                st.divider()
-                                
-                                # Display results from search_result_div_2
-                                try:
-                                    link = next(manga_links)
-                                    href = link.get("href")
-                                    if "chapter" not in href:
-                                        cch = f"{href}chapter-1/"
-                                    else:
-                                        cch = href
-                                    manga_name=href.split('https://manhuaaz.com/manga/')[1]
-                                   
-                                    img_tag = link.find("img")
-                                    original_string = cch
-                                    obfuscated_text, mapping = obfuscate(original_string)
-                                    if img_tag:
-                                        st.write(f"[{manga_name}]({cch})")
-                                        img_url = img_tag.get("data-src")
-                                        st.image(img_url, caption=obfuscated_text, use_column_width='always')
-                                        
-                                        txt = f"""
-                                        {obfuscated_text}
-                                        """
-                                        st.code(txt, language='java')
-                                        st.caption('Copy Code')
-                                        st.divider()
-                                except StopIteration:
-                                    break
-
-asyncio.run(search_function())
+                                    st.caption('Copy Code')
+                                    st.divider()
+                            except StopIteration:
+                                break
 
 col1, col2, col3 = st.columns(3)
 outer_cols = st.columns([1, 2])
